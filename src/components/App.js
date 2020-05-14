c01687771fdd0b3af6cbf1d06e309e431efc3855
Vlad Kyshkan <vladkyshkan@gmail.com>
2020-05-14 12:45:22 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <div>
+      <h1>ML + Design</h1>
+    </div>
+  );
+}
+
+export default App;
